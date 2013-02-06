e004114fca5ca05fdb08722a797f756685118769
Kazuhiko Shirakawa <kazuhiko.shirakawa@neu-e.biz>
2013-02-06 16:20:36 +0900

    [#43911287] update validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
+
+  validates_presence_of :name, :description, :price, :image
+  validates_uniqueness_of :name
+  validates_numericality_of :price, :only_integer => true
 end
