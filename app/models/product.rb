875728e4129b3aa6506ae7b6b6ea8e4e5eaa1c41
Kazuhiko Shirakawa <kazuhiko.shirakawa@neu-e.biz>
2013-02-06 16:26:49 +0900

    [#43911287] update validation must be a multiple hundreds

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,13 @@ class Product < ActiveRecord::Base
   validates_presence_of :name, :description, :price, :image
   validates_uniqueness_of :name
   validates_numericality_of :price, :only_integer => true
+
+  validate :price_must_be_amultiple_of_hundreds
+
+  private
+  def price_must_be_amultiple_of_hundreds
+    unless price % 100 == 0
+      errors.add :price, "must be a multiple of hundreds"
+    end
+  end
 end
