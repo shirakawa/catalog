e461059c71aa9ff78de1d153e82aba2b1ac9b0d1
Kazuhiko Shirakawa <kazuhiko.shirakawa@neu-e.biz>
2013-02-06 15:00:44 +0900

    [#43908949] update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206044257) do
+ActiveRecord::Schema.define(:version => 20130206055635) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130206044257) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "price"
+    t.string   "image"
   end
 
 end
